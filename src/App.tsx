dd791f37d9d59542b7e76a9e458cfcb2da416b2d
Mr-Verse <hdmail010@gmail.com>
2023-09-14 00:47:37 +0530

    Building Responsive Layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
+
 function App() {
-  return null;
+  const navBarHeight = "64px";
+
+  return (
+    <Grid templateColumns={{base:"200px 1fr", xl:"300px 1fr"}}>
+      <GridItem
+        position={{ base: "fixed", md: "static" }}
+        colSpan={2}
+        bg="orange"
+        h={navBarHeight}
+        width="100%"
+      >
+        NavBar
+      </GridItem>
+      <Show above="md">
+        <GridItem
+          mt={{ base: navBarHeight, md: 0 }}
+          bg="purple.400"
+          height={`calc(100vh - ${navBarHeight})`}
+          overflowY="auto"
+        >
+          <Text>Aside</Text>
+        </GridItem>
+      </Show>
+      <GridItem
+        colSpan={{ base: 2, md: 1 }}
+        mt={{ base: navBarHeight, md: 0 }}
+        height={`calc(100vh - ${navBarHeight})`}
+        overflowY="auto"
+        bg="blue.300"
+      >
+        <Text>Main</Text>
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
