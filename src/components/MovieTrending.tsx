97755e7b0a0536970c506114fae19c78c76df5d5
Mr-Verse <hdmail010@gmail.com>
2023-09-15 21:49:35 +0530

    Adding Loading Skeletons (MovieCardSkeleton) for MovieCard loading in MoviesTrending component.

--- src/components/MovieTrending.tsx
+++ src/components/MovieTrending.tsx
@@ -1,13 +1,15 @@
-import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid , Text } from "@chakra-ui/react";
 import MovieCard from "./MovieCard";
 import { Interval, useMoviesTrending } from "../hooks/useMoviesTrending";
+import MovieCardSkeleton from "./MovieCardSkeleton";
 
 interface Props {
   interval: Interval;
 }
 
 const MoviesTrending = ({ interval }: Props) => {
-  const { moviesTrending, error } = useMoviesTrending(interval);
+  const { moviesTrending, error, isLoading } = useMoviesTrending(interval);
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <Box px="8px">
@@ -19,11 +21,13 @@ const MoviesTrending = ({ interval }: Props) => {
           ? "This Week"
           : "This Month"}
       </Heading>
-      {error && <p>{error}</p>}
+      {error && <Text color="red">{error}</Text>}
       <SimpleGrid
         columns={{ base: 2, sm: 3, md: 3, lg: 5, xl: 6 }}
         spacing={{ base: "12px" }}
       >
+        {isLoading &&
+          skeletons.map((Skeleton) => <MovieCardSkeleton key={Skeleton} />)}
         {moviesTrending.map((movie) => (
           <MovieCard key={movie.ids.simkl_id} movie={movie} />
         ))}
